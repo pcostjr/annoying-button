13a2c8cb312c142fe78af6015aaa1def2c167bdf
Peter Costantino <pcostantino@acitech.org>
2024-02-29 08:17:10 -0500

    added movement changes

--- script.js
+++ script.js
@@ -65,3 +65,24 @@ function onButtonClick() {
 
 }
 
+function annoy() {
+
+    // conditional ladder that modifies button's behaviors based off of the number of times it's been pressed
+    if(annoyed == 1)
+      alert("Stop That!") 
+    else if(annoyed == 5)
+      alert("I said stop that!")
+    else if(annoyed == 10)
+    {
+      var greenbutton = document.createElement("BUTTON")
+      greenbutton.appendChild(document.createTextNode("PRESS"))
+      greenbutton.classList.add("greenbutton")
+      playarea.appendChild(greenbutton)
+    }
+    else if(annoyed == 15)
+    {
+      redvoice.innerText = "are you serious?"
+    }
+    // increments annoyed
+    annoyed++
+  }
