ef54453b76df2fbf64e21bc8910d0b1314365960
Peter Costantino <pcostantino@acitech.org>
2024-02-29 08:14:14 -0500

    kl;asdfjklasdf

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-// script.js
-// Annoying Button
-// Mr. Costantino
-// 2.22.24
+// Annoying Button | script.js
+// Mr. Costantino  | February 22nd, 2024
+
 
 // get document elements
-const redbutton = document.getElementById("redbutton");
+const mainbutton = document.getElementById("mainbutton");
+const speech = document.getElementById("speech");
+const playarea = document.getElementById("playarea");
+
+// variables for tracking clicks
+let clicks = 0;
+let coming = 25;
 
 // add event listeners
-redbutton.addEventListener('click', () => onButtonClick());
+mainbutton.addEventListener('click', () => onButtonClick());
+
 
 // main driver for annoying button
 function onButtonClick() {
 
-    alert("Hey! Stop That!");
+    clicks++;
+    if(clicks == 1)
+    {
+        speech.innerText = "DO NOT PRESS";
+    }
+    else if(clicks == 5)
+    {
+        speech.innerText = "Not cool...";
+        let newbutton = document.createElement("BUTTON");
+        newbutton.textContent = "PRESS";
+        newbutton.classList.add('mainbutton');
+        newbutton.classList.add('green');
+        playarea.appendChild(newbutton);
+    }
+    else if(clicks == 10)
+    {
+        speech.innerText = "You're not very good at following directions, are you?";
+    }
+    else if(clicks == 15)
+    {
+        speech.innerText = "10.52.0.7";
+    }
+    else if(clicks == 20)
+    {
+        speech.innerText = "Your family is not safe.";
+    }
+    else if(clicks == 25)
+    {
+        speech.innerText = "I am heading towards your location.";
+    }
+    else if(clicks >= 25 && clicks <= 51)
+    {
+        speech.innerText = "I am. " + coming + "ft. away from your location.";
+        coming--;
+    }
+    else if(clicks == 52)
+    {
+        mainbutton.classList.add('.angry');
+        document.body.classList.add(".angrybody");
+    }
+    console.log(clicks);
+
+
+
 
 }
 
